21a9611c79ae60e98c5a522501849d576051a9ad
CITYC\Wernerm <wernerm@citytech.co.za>
2022-07-15 09:52:54 +0200

    presentational stateless components

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PassengerDashboardModule } from './passenger-dashboard/containers/passenger-dashboard.module';
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 @NgModule({
   declarations: [
