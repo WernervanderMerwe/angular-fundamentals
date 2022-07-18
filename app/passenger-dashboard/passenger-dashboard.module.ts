335fbe10422045cfdedbbe7ee18bd5309b1c7995
CITYC\Wernerm <wernerm@citytech.co.za>
2022-07-18 12:05:46 +0200

    @injectable

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
@@ -13,7 +14,8 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
     PassengerDetailComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpModule
   ],
   exports: [
     PassengerDashboardComponent
