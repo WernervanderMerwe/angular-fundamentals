f4550de593c124b4a9a903cefed9524947f7133e
CITYC\Wernerm <wernerm@citytech.co.za>
2022-07-19 10:49:59 +0200

    styling active routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   template: `
     <div class="app">
       <!-- <passenger-viewer></passenger-viewer> without router -->
+      <nav class="nav">
+        <a
+          routerLink="/"
+          routerLinkActive="active"
+          [routerLinkActiveOptions]="{ exact: true }">
+          Home
+        </a>
+        <a
+          routerLink="/oops"
+          routerLinkActive="active">
+          404
+        </a>
+      </nav>
       <router-outlet></router-outlet>
     </div>
   `
