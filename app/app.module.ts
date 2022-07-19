d6f6781dc165e8bed38f1229f26cfe2d090c374c
CITYC\Wernerm <wernerm@citytech.co.za>
2022-07-19 13:03:39 +0200

    implimenting a redirect

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,7 +9,7 @@ import { NotFoundComponent } from './not-found.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'passengers', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ]
 
@@ -22,7 +22,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     CommonModule,
-    RouterModule.forRoot(routes, { useHash: true }),
+    RouterModule.forRoot(routes),
     PassengerDashboardModule
   ],
   bootstrap: [AppComponent]
